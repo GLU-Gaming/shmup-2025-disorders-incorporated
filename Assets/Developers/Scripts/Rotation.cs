f5294d05d65d2167f6980e892c5193958a038b44
Xanoblade404 <lauranvantveer@gmail.com>
2025-04-02 11:05:18 +0200

    Pick ups
    asd

--- Assets/Developers/Scripts/Rotation.cs
+++ Assets/Developers/Scripts/Rotation.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+
 public class RotateObject : MonoBehaviour
 {
     public float rotationSpeed = 50f; // Speed of rotation
     public bool clockwise = true; // Determines rotation direction
 
+    public enum RotationAxis { X, Y, Z } // Enum for axis selection
+    public RotationAxis rotationAxis = RotationAxis.Y; // Default to Y-axis
+
     void Update()
     {
         float direction = clockwise ? 1f : -1f;
-        // Change Vector3.right to the appropriate axis:
-        // Vector3.up for Y-axis rotation
-        // Vector3.forward for Z-axis rotation
-        // Vector3.right for X-axis rotation
-        transform.Rotate(Vector3.up * rotationSpeed * direction * Time.deltaTime);
+        Vector3 axis = Vector3.zero;
+
+        // Set rotation axis based on selection
+        switch (rotationAxis)
+        {
+            case RotationAxis.X:
+                axis = Vector3.right;
+                break;
+            case RotationAxis.Y:
+                axis = Vector3.up;
+                break;
+            case RotationAxis.Z:
+                axis = Vector3.forward;
+                break;
+        }
+
+        transform.Rotate(axis * rotationSpeed * direction * Time.deltaTime);
     }
 }
